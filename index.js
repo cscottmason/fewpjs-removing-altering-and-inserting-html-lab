2d87c179a5d320f07e3d1b5a3618be4e67c62b40
Candice Scott-Mason <cscottmason23@gmail.com>
2021-04-06 18:53:52 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+document.querySelector('main').remove()
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory')
+newHeader.innerHTML = "Candice is the champion"
+
+document.body.appendChild(newHeader)
